e71c559811f45e5940dac782a3402a04e02ed5f4
eshwar-blenheim <138447852+eshwar-blenheim@users.noreply.github.com>
2023-08-07 15:37:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import os
 # Load the dataset (replace 'your_dataset.csv' with the actual file path)
 df = pd.read_csv('Research_paper.csv')
 
+
 # Function to analyze user input using GPT-3.5 Turbo
 def search_titles(topic):
     topic = topic.lower()
     matches = df[df['Domain'].str.lower().str.contains(topic)]
     return matches[['Title', 'Abstract']]
+
 
 # Main function to run the chatbot
 def main():
@@ -32,32 +34,34 @@ def main():
     If the Domain Name is not in the above topics please return the following text:
     "Sorry I dont have research papers on this topic. I will make them available soon."
     '''
-    
+
     if user_input and api_key:
         conversation = [
             {"role": "system", "content": sys_prp},
             {"role": "user", "content": user_input}
         ]
-        
+
         # Create OpenAI GPT-3.5 Turbo response
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=conversation,
             temperature=0,
-            max_tokens=70,
+            max_tokens=150,
         )
         analysis_result = response['choices'][0]['message']['content']
-        
+
         # Display analysis result
         st.text_area("Analysis Result:", analysis_result, height=100)
-        
+
         # Search for titles in the domain column
         titles = search_titles(analysis_result)
-        if titles.empty():
+        if titles.empty:
             st.write("No matching titles found.")
         else:
-            st.subheader("As per your request, I am pleased to submit the following research papers for your consideration:")
-            st.table(titles.to_html())
+            st.subheader(
+                "As per your request, I am pleased to submit the following research papers for your consideration:")
+            st.table(titles)
+
 
 if __name__ == "__main__":
     main()
