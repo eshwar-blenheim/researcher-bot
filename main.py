03988f43d56e66c0e561927ee7c3cebf5476d300
eshwar-blenheim <138447852+eshwar-blenheim@users.noreply.github.com>
2023-08-07 11:50:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import openai
+import os
+
+# Load the dataset (replace 'your_dataset.csv' with the actual file path)
+df = pd.read_csv('Research_paper.csv')
+
+# Function to analyze user input using GPT-3.5 Turbo
+def search_titles(topic):
+    topic = topic.lower()
+    matches = df[df['Domain'].str.lower().str.contains(topic)]
+    return matches['Title'].tolist()
+
+# Main function to run the chatbot
+def main():
+    st.title("Research Paper Chatbot")
+
+    # OpenAI API key input
+    st.write("Enter your OpenAI API key:")
+    api_key = st.text_input("API Key:")
+    openai.api_key = api_key
+
+    # User input
+    st.write("How may I help you?")
+    user_input = st.text_input("Please choose your Domain", "")
+    sys_prp = '''
+    You have been given the following user_input,
+    Analyze the text and find whether the text is about the following domains: 
+    Health or Artificial Intelligence or Machine Learning or Mathematics.
+    Return only the Domain Name
+    '''
+    
+    if user_input and api_key:
+        conversation = [
+            {"role": "system", "content": sys_prp},
+            {"role": "user", "content": user_input}
+        ]
+        
+        # Create OpenAI GPT-3.5 Turbo response
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+            temperature=0,
+            max_tokens=150,
+        )
+        analysis_result = response['choices'][0]['message']['content']
+        
+        # Display analysis result
+        st.text_area("Analysis Result:", analysis_result, height=100)
+        
+        # Search for titles in the domain column
+        titles = search_titles(analysis_result)
+        
+        if titles:
+            st.subheader("Matching Titles:")
+            for title in titles:
+                st.write(title)
+        else:
+            st.write("No matching titles found.")
+
+if __name__ == "__main__":
+    main()
